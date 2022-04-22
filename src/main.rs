83a7d595bc96e921091e30974831cb0c1bb9f270
Shuntaro Nishizawa <me@shun.technology>
2022-04-22 18:13:14 +0900

    feat: Add serenity

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::model::gateway::Ready;
+use serenity::prelude::*;
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content != "!ping" {
+            return;
+        }
+
+        if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+            println!("Error sending message: {:?}", why);
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let token = env::var("DISCORD_TOKEN")?;
+
+    let intents = GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await?
+        .start()
+        .await?;
+
+    Ok(())
 }
